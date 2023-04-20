1ee3a1cae491defeaf21b3dcd4382901eb482966
Conor McCabe <mccabeconor941@gmail.com>
2023-04-20 18:50:45 +0800

    first-commit

---
+++ Leaflet-Part-1/static/js/logic.js
@@ -0,0 +1,11 @@
+// Create the tile layer that will be the background of our map.
+let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+// Creating the map object
+let myMap = L.map("map", {
+    center: [40.7, -73.95],
+    zoom: 11
+  });
+  
