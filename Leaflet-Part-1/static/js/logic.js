fb3f8c6b5f128ad02ea925c16ef491ddc273d6f4
Conor McCabe <mccabeconor941@gmail.com>
2023-04-21 11:46:34 +0800

    map

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,5 +1,5 @@
   // Create the tile layer that will be the background of our map.
-let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
@@ -26,8 +26,46 @@ let myMap = L.map("map", {
   let overlayMaps = {
     earthquake: earthquake
   };
-  
 
+  // adding a control so you can adjust layers
+  L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+
+  // set the style data for each earthquake included on the map.
+  function styleInfo(features) {
+    return {
+      fillOpacity: 1,
+      fillcolor: getColor(features.geometry.coordinates[2]),
+      color: "#000000",
+      radius: getRadius(features.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+// This function sets the circle colour for the earthquake depending on its depth
+function getColour(depth) {
+  if (depth > 90) {
+      return "#19073B";
+  }
+  else if (depth > 70) {
+      return "#573172";
+  }
+  else if (depth >50) {
+      return "#804E74";
+  }
+  else if (depth > 30) {
+      return "#A96A77";
+  }
+  else if (depth > 10) {
+      return "#D18779";
+  }
+  else if (depth > -10){
+  return "#F29455";
+  }
+  else {
+  return "#FF51EB"  
+  }
+};
 
 // Perform a request to get the query URL
 d3.json(url).then(function(data) {
